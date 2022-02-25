39a8c5b71769ed48ff864ba11cf4b2db601c6346
Qiz <qizpham@gmail.com>
2022-02-25 22:51:13 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Body" >
+        <div className="Container">
+            <h2>Login From</h2>
+            
+            <div className='btnLoginorSingup'>
+                <button>Login</button>
+                <button>Singup</button>
+            </div>
+
+            <div className='input'>
+              <input type='text' name='email' placeholder='Email address' />
+              <input type='text' name='password' placeholder='Password' />
+            </div>
+
+            <div className='footer'>
+              <button>Login</button>
+              <p>Not a member
+                  <a href='#'>Signup now</a>
+              </p>
+            </div>
+        </div>
+      
     </div>
   );
 }
